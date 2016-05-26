0636d52f3956f140a4e3edb3be057a4156c64a0c
Christian M <cwmct79@gmail.com>
2016-05-26 11:53:46 +0200

    Create orders controller and views, and update about view

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+  # You can have the root of your site routed with "root"
+  root 'static_pages#index'
+
+  get 'static_pages/index'
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
-  get 'static_pages/index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
