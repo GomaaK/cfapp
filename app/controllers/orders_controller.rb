0976609314d6750d7a877363a0edfdc3632119a5
Christian M <cwmct79@gmail.com>
2016-05-31 12:43:15 +0200

    Add partial for orders and make final appearance changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,20 +5,4 @@ class OrdersController < ApplicationController
 
   def show
   end
-
-  def new
-    @order = Order.new
-  end
-
-  def create
-    @order = Order.new(order_params)
-  end
-
-  def destroy
-  end
-
-  private
-  def order_params
-      params.require(:order).permit(:user_id, :product_id, :total)
-  end
 end
