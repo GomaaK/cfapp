f521e06bd9e1c377ac23751902c63f6cd86a6338
Christian M <cwmct79@gmail.com>
2016-06-03 11:40:50 +0200

    Update contact form and add ActionMailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,4 +8,17 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+	def thank_you
+		@name = params[:name]
+		@company = params[:company]
+		@email = params[:email]
+		@telephone = params[:telephone]
+		@message = params[:message]
+		ActionMailer::Base.mail(
+			:from => @email,
+ 			:to => 'chrwimct@gmail.com',
+ 			:subject => "A new contact form message from #{@name}",
+			:body => @message).deliver_now
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
+  post 'static_pages/thank_you'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
