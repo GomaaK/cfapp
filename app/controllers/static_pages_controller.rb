d1a1d1c25884045793fddb8d45af010d3896fd88
Christian M <cwmct79@gmail.com>
2016-06-06 17:18:45 +0200

    Add icons to index page and update contact form submission mail

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,18 +15,14 @@ class StaticPagesController < ApplicationController
   def contact
   end
 
-  # Contact form submission email sent to me
+  # Contact form submission email sent to me and sender
 	def thank_you
 		@name = params[:name]
 		@company = params[:company]
 		@email = params[:email]
 		@telephone = params[:telephone]
 		@message = params[:message]
-		ActionMailer::Base.mail(
-			:from => @email,
- 			:to => 'cwmct79@gmail.com',
- 			:subject => "New Website Form Message",
-			:body => [@name, @company, @email, @telephone, @message]).deliver_now
+    UserMailer.contact_info(@name, @company, @email, @telephone, @message).deliver_now
 		UserMailer.contact_reply(@email, @name, @message).deliver_now
 	end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,17 @@
 class UserMailer < ApplicationMailer
 	default from: "cwmct79@gmail.com"
 
+# Contact form submission information email to me
+	def contact_info(name, company, email, telephone, message)
+		@name = name
+		@company = company
+  	@email = email
+  	@telephone = telephone
+  	@message = message
+    mail(:to => "cwmct79@gmail.com",
+        :subject => "Oasis Books Contact Form Message")
+  end
+
 	# Contact form submission email reply to sender
 	def contact_reply(email, name, message)
   	@email = email
