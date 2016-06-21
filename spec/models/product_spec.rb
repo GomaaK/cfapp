4570c3f3e496b55ba9f02c1f580e7a215455b309
Christian M <cwmct79@gmail.com>
2016-06-21 09:53:28 +0200

    Add rspec product unit tests and rspec model files

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe Product do
+	context "when the product has comments" do
+		before do
+  		@product = Product.create!(title: "Example Book", author: "John Doe", price: 10.00, image_url: "products/example_book.jpg")
+  		@user = User.create!(email: "example@user.com", password: "exampleuser1")
+  		@product.comments.create!(rating: 1, user: @user, body: "Terrible book!")
+  		@product.comments.create!(rating: 3, user: @user, body: "Good book!")
+  		@product.comments.create!(rating: 5, user: @user, body: "The best book!")
+		end
+
+		it "returns the average rating of all comments" do
+			expect(@product.average_rating).to eq 3
+		end
+	end
+
+	context "when the product is not valid" do
+		before do
+			Product.new(description: "Interesting book")
+		end
+
+		it "returns an invalid product message" do
+			expect(Product.new(description: "Interesting book")).not_to be_valid
+		end
+	end
+
+end
